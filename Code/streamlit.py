f67cb73677a0c37b7f1e2586d673934383c5ecd7
Connor Phillips <connorphillips@connors-mbp.myfiosgateway.com>
2023-05-15 08:12:17 -0400

    Update 7:32 am, app deployment

--- Code/streamlit.py
+++ Code/streamlit.py
@@ -39,7 +39,7 @@ if p_type == "Pitcher":
 
     
 #Reading in relevant data, batter or pitching career data based on user input
-df_career = pd.read_csv(f"../Data/career_{p_type.lower()}.csv")
+df_career = pd.read_csv(f"../main/Data/career_{p_type.lower()}.csv")
 
 #Intializes data frames for X and y for linear regression
 X = pd.DataFrame()
